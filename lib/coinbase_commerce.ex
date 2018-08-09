cd868a14d6b7715fce5291ff213a42d9e120787a
Andrew Allen <allenan@helium.com>
2018-08-09 15:42:24 -0700

    initial commit

---
+++ lib/coinbase_commerce.ex
@@ -0,0 +1,18 @@
+defmodule CoinbaseCommerce do
+  @moduledoc """
+  Documentation for CoinbaseCommerce.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CoinbaseCommerce.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/coinbase_commerce_test.exs
@@ -0,0 +1,8 @@
+defmodule CoinbaseCommerceTest do
+  use ExUnit.Case
+  doctest CoinbaseCommerce
+
+  test "greets the world" do
+    assert CoinbaseCommerce.hello() == :world
+  end
+end
